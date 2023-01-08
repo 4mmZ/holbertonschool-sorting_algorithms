8d867c4bc370df85ee8b6151c06833ff669f95d6
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-08 14:41:21 -0800

    Task 02

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * selection_sort - In a function that sorts an array of
+ * integers in ascending order using the Selection sort algorithm
+ * @array: array
+ * @size: size_t
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j = 0;
+	int aux, min = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		aux = array[i];
+		array[i] = array[min];
+		array[min] = aux;
+		print_array(array, size);
+	}
+}
